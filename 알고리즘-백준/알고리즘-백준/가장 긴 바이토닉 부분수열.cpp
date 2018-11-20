1c13f3c958619e593918792ea7ac7a809cb92c76
woosuk6549 <woosuk6549@naver.com>
2018-11-20 15:58:20 +0900

    커밋

---
+++ 알고리즘-백준/알고리즘-백준/가장 긴 바이토닉 부분수열.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+
+int S[1001], cache[1001];
+int n;
+bool change = false;
+int func(int start) {
+	int& ret = cache[start];
+	if (ret != -1)return ret;
+
+	ret = 1;
+	for (int next = start + 1; next < n; ++next) {
+		if (S[start] < S[next]&&change==false) {
+			ret = max(ret,func(next)+1);
+		}
+		if (S[start] > S[next]) {
+			change = true;
+			ret = max(ret, func(next) + 1);
+			change = false;
+		}
+	}
+	return ret;
+}
+
+int main() {
+	
+
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		cin >> S[i];
+
+	int max_result = 0;
+	int result;
+	for (int i = 0; i < n; ++i) {
+		memset(cache, -1, sizeof(cache));
+		result = func(i);
+		if (max_result < result)
+			max_result = result;
+	}
+	cout << max_result << endl;
+	return 0;
+}
