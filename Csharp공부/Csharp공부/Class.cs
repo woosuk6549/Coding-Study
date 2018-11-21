349d6ba97b096791e27c89706ad8eb3f6eff505b
woosuk6549 <woosuk6549@naver.com>
2018-11-21 17:43:15 +0900

    메소드

---
+++ Csharp공부/Csharp공부/Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Global
+{
+    public static int count = 0;
+}
+class ClassA
+{
+    public ClassA()
+    {
+        Global.count++;
+    }
+}
+class ClassB
+{
+    public ClassB()
+    {
+        Global.count++;
+    }
+}
+
+class MainApp
+{
+    static void Main()
+    {
+        Console.WriteLine("Global count : {0}", Global.count);
+
+        new ClassA();
+        new ClassB();
+        new ClassB();
+        new ClassA();
+
+        Console.WriteLine("Global count : {0}", Global.count);
+
+    }
+}
--- Csharp공부/Csharp공부/Method.cs
+++ Csharp공부/Csharp공부/Method.cs
@@ -11,9 +11,33 @@ namespace Method
         {
             return a - b;
         }
+        
     }
     class MainApp
     {
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        public static void Divide(int a,int b,out int quotient,out int remainder)
+        {
+            quotient = a / b;
+            remainder = a % b;
+        }
+        static int Sum(params int[] args)
+        {
+            int sum = 0;
+            for(int i = 0; i < args.Length; ++i)
+            {
+                if (i > 0) Console.Write(", ");
+                Console.Write(args[i]);
+                sum += args[i];
+            }
+            Console.WriteLine();
+            return sum;
+        }
         public static void Main()
         {
             int result = Calculator.Plus(10, 23);
@@ -21,6 +45,19 @@ namespace Method
 
             result = Calculator.Minus(100, 32);
             Console.WriteLine(result);
+
+            int x = 3, y = 4;
+            Console.WriteLine("x: {0}, y: {1}", x, y);
+            Swap(ref x, ref y);
+            Console.WriteLine("x: {0}, Y: {1}", x, y);
+
+            int quo=0, rem=0;
+            int a = 30, b = 7;
+            Divide(a, b, out quo, out rem);
+            Console.WriteLine("{0},{1}", quo, rem);
+
+            int sum = Sum(3, 4, 5, 6, 7, 8, 9);
+            Console.WriteLine(sum);
         }
     }
 }
