5af5a38581a5a2abcc1b4c76b25aee21b8d6b566
woosuk6549 <woosuk6549@naver.com>
2018-11-25 01:45:24 +0900

    커밋

--- Csharp공부/Csharp공부/Class.cs
+++ Csharp공부/Csharp공부/Class.cs
@@ -59,29 +59,98 @@ class Derived : Base
         Console.WriteLine("{0}.Derived()", this.Name);
     }
 }
+//is,as
+class Mammal
+{
+    public void Nurse()
+    {
+        Console.WriteLine("Nurse()");
+    }
+}
+class Dog : Mammal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Bark()");
+    }
+}
+class Cat : Mammal
+{
+    public void Meow()
+    {
+        Console.WriteLine("Meow()");
+    }
+}
+
+//오버라이딩
+class ArmorSuite {
+    public virtual void Initialize()
+    {
+        Console.WriteLine("Armored");
+    }
+}
+class Ironman : ArmorSuite
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        Console.WriteLine("repulsor rays armored");
+    }
+}
+
+class Base2
+{
+    public void MyMethod()
+    {
+        Console.WriteLine("Base.mymethod()");
+    }
+}
+class Derived2 : Base2
+{
+    public new void MyMethod()
+    {
+        Console.WriteLine("Derived.mymethod()");
+    }
+}
 class MainApp
 {
     static void Main()
     {
         Console.WriteLine("Global count : {0}", Global.count);
-
         new ClassA();
         new ClassB();
         new ClassB();
         new ClassA();
-
         Console.WriteLine("Global count : {0}", Global.count);
 
 
         MyClass a = new MyClass();
         a.printField();
-
         MyClass b = new MyClass(23);
         b.printField();
-
         MyClass c = new MyClass(10, 20);
         c.printField();
-
         Derived d = new Derived("asdf");
+
+
+        Mammal mammal = new Dog();
+        Dog dog;
+
+        if(mammal is Dog)
+        {
+            dog = (Dog)mammal;
+            dog.Bark();
+        }
+
+        Mammal mammal2 = new Cat();
+        Cat cat = mammal2 as Cat;
+        if (cat != null)
+            cat.Meow();
+
+        Cat cat2 = mammal as Cat;
+        if (cat2 != null)
+            cat2.Meow();
+        else
+            Console.WriteLine("cat2 is not a  cat");
     }
 }
