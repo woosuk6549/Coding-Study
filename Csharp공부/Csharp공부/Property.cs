5cc3f7991a0fc7d637786a1d8e79392503bd0e25
woosuk6549 <woosuk6549@naver.com>
2018-11-28 01:23:51 +0900

    프로퍼티

---
+++ Csharp공부/Csharp공부/Property.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp공부
+{
+    class BirthdayInfo
+    {
+        private string name;
+        private DateTime birthday;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public DateTime Birthday
+        {
+            get;set;
+        }
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(birthday).Ticks).Year;
+            }
+        }
+    }
+    class Property
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            birth.Name = "아서";
+            birth.Birthday = new DateTime(1991, 6, 28);
+
+            Console.WriteLine("Name : {0}", birth.Name);
+            Console.WriteLine("birthday : {0}", birth.Birthday);
+            Console.WriteLine("Age : {0}", birth.Age);
+        }
+    }
+}
